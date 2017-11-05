1804a70f8baaf1bf07442e60439879cc16627f3e
ericyu423 <ericyu423@hotmail.com>
2017-11-05 13:41:56 -0800

    bundle (read existing files in your app bundle)

--- FileManger/ViewController.swift
+++ FileManger/ViewController.swift
@@ -8,18 +8,61 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITextViewDelegate {
+    
+    @IBOutlet weak var dailyTask: UITextView!
+   
+    var pathAndText: (String,URL)!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        dailyTask.delegate = self
+        pathAndText = prepareFileManger(fileName: "myPlan.txt",text: "")
+        dailyTask.text = pathAndText.0
+        
+        
+      
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func textViewDidChange(_ textView: UITextView) {
+        
+        
+        if let text = dailyTask.text {
+            
+            
+            writeToFileWith(fileURL: pathAndText.1, text: text)
+        }
+        
     }
-
-
+    
+   func writeToFileWith(fileURL: URL,text: String){
+        do{
+            try text.write(to: fileURL, atomically: true, encoding: .utf8)
+        } catch {
+            print("error")
+        }
+    }
+    
+    func prepareFileManger(fileName: String,text: String) -> (String,URL){
+        //get userDirectory path
+        let manager = FileManager.default
+        let document = manager.urls(for: .documentDirectory, in: .userDomainMask)
+        let docURL = document.first!
+        
+        //read return file content or create blank file
+        let fileURL = docURL.appendingPathComponent(fileName)
+        
+        if manager.fileExists(atPath: fileURL.path) {
+            if let content = manager.contents(atPath: fileURL.path),let text = String(data: content, encoding: .utf8){
+                
+                return (text,fileURL)
+            }else {
+                 manager.createFile(atPath: fileURL.path, contents: nil, attributes: nil)
+            }
+        }
+             return ("",fileURL)
+    }
+        
+   
 }
+
 
